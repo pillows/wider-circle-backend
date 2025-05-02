076ae52e38e37c4d2f3e1d5a244bfd6362e9312d
Matthew Wong <matthew.wong@outlook.com>
2025-05-02 03:34:55 -0400

    Cleaned up main.go file and added gitignore file

--- main.go
+++ main.go
@@ -47,20 +47,9 @@ func main() {
 	port := getEnv("PORT", "8080")
 
 	// base case employee URL
+	// see env file other test case urls
 	employeeURL := getEnv("EMPLOYEE_SEED_URL",
 		"https://gist.githubusercontent.com/chancock09/6d2a5a4436dcd488b8287f3e3e4fc73d/raw/fa47d64c6d5fc860fabd3033a1a4e3c59336324e/employees.json")
-
-	// test case 1 with an extra nested employee
-	// employeeURL := getEnv("EMPLOYEE_SEED_URL",
-	// 	"https://gist.githubusercontent.com/pillows/dded29423234b68f3cdde47aeafb0d63/raw/ac706aa7ef6c28e2d5bcc78b1560f621c98778e3/gistfile1.json")
-
-	// test case 2 with no employees
-	// employeeURL := getEnv("EMPLOYEE_SEED_URL",
-	// "https://gist.githubusercontent.com/pillows/dbca826d3821805a842efb39315591e1/raw/9ffcd84a87536977082a68a99513270b75714a39/empty.json")
-
-	// test case 3 with a single employee
-	// employeeURL := getEnv("EMPLOYEE_SEED_URL",
-	// 	"https://gist.githubusercontent.com/pillows/dded29423234b68f3cdde47aeafb0d63/raw/28d7fd2b3535d88d56738c459dac630036d676a8/gistfile1.json")
 
 	database, err := db.InitializeDatabase(dbDriver, dbDSN)
 	if err != nil {
